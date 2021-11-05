1737473c45fd509a59c7bffd73f639249b7997ae
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2021-11-05 18:49:55 +0900

    feat faucet FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,96 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ethers, BigNumberish } from "ethers";
+import GameItemsJson from "../../artifacts/contracts/GameItems.sol/GameItems.json";
+import { GameItems } from "../../typechain/GameItems";
+
+const contractAddress = "0x0165878a594ca255338adfa4d48449f69242eb8f";
+
+const getGameItemsContract = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    contractAddress,
+    GameItemsJson.abi,
+    signer
+  ) as GameItems;
+
+  return contract;
+};
+
+const useCurrentAddress = () => {
+  const [currentAddress, setCurrentAddress] = useState<string>(
+    window.ethereum.selectedAddress
+  );
+
+  useEffect(() => {
+    window.ethereum.on("accountsChanged", ([newAddress]: string[]) => {
+      setCurrentAddress(newAddress);
+    });
+  }, []);
+
+  return { currentAddress };
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [balance, setBalance] = useState("0");
+  const { currentAddress } = useCurrentAddress();
+
+  useEffect(() => {
+    (async () => {
+      if (currentAddress) {
+        const contract = getGameItemsContract();
+        const b = await contract.balanceOf(currentAddress, 0);
+        setBalance(b.toString());
+      }
+    })();
+  }, [currentAddress]);
+
+  const getToken = async (tokenId: BigNumberish, amount: number) => {
+    const contract = getGameItemsContract();
+    const tx = await contract.faucet(tokenId, amount);
+    await tx.wait();
+    const b = await contract.balanceOf(currentAddress, 0);
+    setBalance(b.toString());
+  };
+
+  // const handleConnect = async () => {
+  //   const [account] = await window.ethereum.request!({
+  //     method: "eth_requestAccounts",
+  //   });
+
+  //   setCurrentAddress(account);
+  // };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(amount);
+    await getToken(0, amount);
+    setAmount(0);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>GameItems Faucet 🚰</h1>
+      {/* <button onClick={handleConnect}>Connect</button> */}
+      <p>{currentAddress}</p>
+      <p>{balance}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          onChange={handleChange}
+        />
+        <button type="submit">GET</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,46 @@
 /// <reference types="vite/client" />
+
+import {
+  ProviderMessage,
+  ProviderRpcError,
+  ProviderConnectInfo,
+  RequestArguments,
+} from "hardhat/types";
+
+export interface EthereumEvent {
+  connect: ProviderConnectInfo;
+  disconnect: ProviderRpcError;
+  accountsChanged: Array<string>;
+  chainChanged: string;
+  message: ProviderMessage;
+}
+
+declare global {
+  interface Window {
+    ethereum: Ethereumish;
+  }
+}
+
+type EventKeys = keyof EthereumEvent;
+type EventHandler<K extends EventKeys> = (event: EthereumEvent[K]) => void;
+
+export interface Ethereumish {
+  autoRefreshOnNetworkChange: boolean;
+  chainId: string;
+  isMetaMask?: boolean;
+  isStatus?: boolean;
+  networkVersion: string;
+  selectedAddress: any;
+
+  on<K extends EventKeys>(event: K, eventHandler: EventHandler<K>): void;
+  enable(): Promise<any>;
+  request?: (request: { method: string; params?: Array<any> }) => Promise<any>;
+  /**
+   * @deprecated
+   */
+  send?: (
+    request: { method: string; params?: Array<any> },
+    callback: (error: any, response: any) => void
+  ) => void;
+  sendAsync: (request: RequestArguments) => Promise<unknown>;
+}
