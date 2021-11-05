9c12233e9287cc8b7575f292c8bccd0b84325a20
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2021-11-05 18:49:41 +0900

    add balance list script

---
+++ scripts/02_get_gameItems_balance.ts
@@ -0,0 +1,28 @@
+import assert from "assert";
+import { ethers } from "hardhat";
+// import GameItemsArtifact from "../artifacts/contracts/GameItems.sol/GameItems.json";
+
+const CONTRACT_ADDRESS = process.env.GAME_ITEMS_ADDRESS;
+
+async function main() {
+  assert(CONTRACT_ADDRESS);
+
+  const [admin, bob, cathy] = await ethers.getSigners();
+  const gameItems = await ethers.getContractAt("GameItems", CONTRACT_ADDRESS);
+
+  const itemId = 0;
+
+  const [ABalance, BBalance, CBalance] = await gameItems.balanceOfBatch(
+    [admin.address, bob.address, cathy.address],
+    [itemId, itemId, itemId]
+  );
+
+  console.log("admin", ABalance.toString());
+  console.log("bob", BBalance.toString());
+  console.log("cathy", CBalance.toString());
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
